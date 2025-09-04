97f896c5ee1419d89939472d2453647b3ab2646e
Anastasiia-Chikrizova <anastasiia.chikrizova@gmail.com>
2025-09-04 13:42:13 +0300

    added page fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: 'Flight-Search-System',
+  base: '/Flight-Search-System/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
